27457eef6e53c919ebc88adb8f281aa928550304
hf16 <helmifarizki231@gmail.com>
2023-01-27 17:08:15 +0700

    Tanpa cross_val_score

--- app.py
+++ app.py
@@ -2,20 +2,17 @@ from flask import Flask, render_template, request
 from sklearn.svm import SVC
 from imblearn.over_sampling import SMOTE
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import make_scorer, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.svm import LinearSVC
 import pandas as pd
 import re
-import nltk
 import csv
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
 import math
-
-# Unduh stopwords dan punkt tokenizer
+import time
+import nltk
 nltk.download('stopwords')
-nltk.download('punkt')
+
 
 app = Flask(__name__)
 
@@ -93,53 +90,49 @@ def preprocessing(data):
 
 @app.route('/')
 def index():
-    # Menampilkan halaman index.html dengan button mulai
-    return render_template('indexEx.html')
+    return render_template('index.html')
 
 
 def dataset():
-    # Membaca file Excel
+    # Membaca file dataset
     df = Dataset_covid
     Z = df['Id'].tolist()
     X = df['Text'].tolist()
     y = df['Label'].tolist()
-
-    # Preprocess the data
     X_preprocessed = [preprocessing(data) for data in X]
-
-    # Flatten the list of tokens into a list of strings
     X_preprocessed = [' '.join(tokens) for tokens in X_preprocessed]
     df_preprocessed = pd.DataFrame(
-        {'Id': Z, 'Text': X_preprocessed, 'Label': y})
-
+        {'Id': Z, 'Text': X_preprocessed, 'Label': y, 'Tweet': X})
     return df_preprocessed
 
 
-@app.route('/result', methods=['POST'])
-def result():
+@app.route('/Dashboard', methods=['POST'])
+def predictResult_svm_smote():
+    start_time_SVM = time.time()
+    start_time_SVM_SMOTE = time.time()
     file = request.files['file']
     uploadData = pd.read_excel(file)
     global Dataset_covid
     Dataset_covid = uploadData
     df_preprocessed = dataset()
     X_preprocessed = df_preprocessed['Text']
-
     train_X, test_X, train_y, test_y = train_test_split(
         df_preprocessed['Text'], df_preprocessed['Label'], train_size=0.8)
-
     # Apply TF-IDF vectorization
     tfidf_vectorizer = TfidfVectorizer(max_features=5000)
     X_train_tfidf = tfidf_vectorizer.fit_transform(train_X)
     X_test_tfidf = tfidf_vectorizer.transform(test_X)
-
-    # Train a SVM
-    svm_no_smote = LinearSVC(C=3)
+    # Model SVM tanpa SMOTE
+    svm_no_smote = LinearSVC()
     svm_no_smote.fit(X_train_tfidf, train_y)
-
-    # Make predictions on the test set using the SVM model without SMOTE
     y_pred_no_smote = svm_no_smote.predict(X_test_tfidf)
     acc_svm = accuracy_score(test_y, y_pred_no_smote)
     acc_svm_1 = round(acc_svm, 4)
+
+    # cv_scores_accuracy = cross_val_score(
+    #     svm_no_smote, X_train_tfidf, train_y, cv=5, scoring='accuracy')
+    # acc_svm_1 = cv_scores_accuracy.mean()
+
     recall_svm = recall_score(test_y, y_pred_no_smote, pos_label='negative')
     recall_svm_1 = round(recall_svm, 4)
     precision_svm = precision_score(
@@ -148,24 +141,15 @@ def result():
     f1_svm = f1_score(
         test_y, y_pred_no_smote, pos_label='negative')
     f1_svm_1 = round(f1_svm, 4)
-
-    kf = KFold(n_splits=5, shuffle=True)
-    cv_scores_SVM = cross_val_score(
-        svm_no_smote, X_train_tfidf, train_y, cv=kf)
-    K_Fold_SVM = round(cv_scores_SVM.mean(), 4)
-
-    # ------------- SVM  SMOTE ---------------------------
+    end_time_SVM = time.time()
+    waktu_komputasi_SVM = end_time_SVM - start_time_SVM
+    # Model SVM dengan Kombinasi SMOTE
     tfidf_vecto = TfidfVectorizer()
     X = tfidf_vecto.fit_transform(X_preprocessed)
-
     smote = SMOTE()
     X_resample, y_resample = smote.fit_resample(X, df_preprocessed['Label'])
-
-    # Split Data
     x_train, x_test, y_train, y_test = train_test_split(
         X_resample, y_resample, train_size=0.8, random_state=42)
-
-    # Modeling
     modelSVM_SMOTE = SVC(kernel='linear', probability=True)
     modelSVM_SMOTE.fit(x_train, y_train)
     predictedSVM_SMOTE = modelSVM_SMOTE.predict(x_test)
@@ -180,87 +164,64 @@ def result():
     f1_svm_smote = f1_score(
         y_test, predictedSVM_SMOTE, pos_label='negative')
     f1_svm_smote_1 = round(f1_svm_smote, 4)
-    # K-FOLD SVM+SMOTE
-    kf = KFold(n_splits=5, shuffle=True)
-    cv_scores_SMOTE = cross_val_score(
-        modelSVM_SMOTE, x_train, y_train, cv=kf)
-    K_Fold_SMOTE = round(cv_scores_SMOTE.mean(), 4)
-    return render_template('resultEx.html', acc_svm=acc_svm_1, recall_svm=recall_svm_1, recall_svm_smote=recall_svm_smote_1, precision_svm=precision_svm_1, precision_svm_smote=precision_svm_smote_1, f1_svm=f1_svm_1, acc_svm_smote=acc_svm_smote_1, f1_svm_smote=f1_svm_smote_1, cv_scores_SVM=cv_scores_SVM, K_Fold_SVM=K_Fold_SVM, cv_scores_SMOTE=cv_scores_SMOTE, K_Fold_SMOTE=K_Fold_SMOTE)
+    end_time_SVM_SMOTE = time.time()
+    waktu_komputasi_SVM_SMOTE = end_time_SVM_SMOTE - start_time_SVM_SMOTE
+    return render_template('dashboardResult.html', acc_svm=acc_svm_1, recall_svm=recall_svm_1, recall_svm_smote=recall_svm_smote_1, precision_svm=precision_svm_1, precision_svm_smote=precision_svm_smote_1, f1_svm=f1_svm_1, acc_svm_smote=acc_svm_smote_1, f1_svm_smote=f1_svm_smote_1, waktu_komputasi_SVM=waktu_komputasi_SVM, waktu_komputasi_SVM_SMOTE=waktu_komputasi_SVM_SMOTE)
 
 
-@app.route('/SVM')
-def SVM():
+@app.route('/tabel_svm')
+def tabel_svm():
     df_preprocessed = dataset()
-    X_preprocessed = df_preprocessed['Text']
-
+    X_preprocessed = df_preprocessed['Tweet']
     train_X, test_X, train_y, test_y = train_test_split(
         df_preprocessed['Text'], df_preprocessed['Label'], train_size=0.8)
-
     # Apply TF-IDF vectorization
     tfidf_vectorizer = TfidfVectorizer(max_features=5000)
     X_train_tfidf = tfidf_vectorizer.fit_transform(train_X)
     X_test_tfidf = tfidf_vectorizer.transform(test_X)
-
-    # Train a SVM
-    svm_no_smote = LinearSVC(C=3)
+    # Model SVM
+    svm_no_smote = LinearSVC()
     svm_no_smote.fit(X_train_tfidf, train_y)
-
-    # Make predictions on the test set using the SVM model without SMOTE
+    # Membuat Hasil Prediksi SVM Tanpa SMOTE
     y_pred_no_smote = svm_no_smote.predict(X_test_tfidf)
-
-    # SVM_DF = pd.DataFrame({'Text': [X_preprocessed[i] for i in range(
-    #     len(test_y))], 'Label': test_y, 'SVM': y_pred_no_smote})
-    SVM_DF = pd.DataFrame({'Text': [X_preprocessed[i] for i in range(
-        len(test_y))], 'Label': [df_preprocessed['Label'][i] for i in range(
-            len(test_y))], 'SVM': y_pred_no_smote})
-
-  # set the number of items per page
+    SVM_DF = pd.DataFrame(
+        {'Text': test_X, 'SVM': y_pred_no_smote})
+    index_to_text_map = {i: X_preprocessed[i]
+                         for i in range(len(X_preprocessed))}
+    SVM_DF.loc[:, 'Text'] = SVM_DF.index.map(
+        index_to_text_map)
+  # Set Peganation
     per_page = 20
     num_pages = math.ceil(SVM_DF.shape[0]/per_page)
-
-    # get the page number from the request's query parameters
     page_number = int(request.args.get('page', 1))
     if page_number > num_pages:
         page_number = num_pages
     elif page_number < 1:
         page_number = 1
-
-    # get the data that should be shown on the current page
     SVM_TABEL = SVM_DF.iloc[per_page*(page_number-1):per_page*page_number]
-    return render_template('SVMEx.html', data=SVM_TABEL, num_pages=num_pages, page_number=page_number)
+    return render_template('tabel_svm.html', data=SVM_TABEL, num_pages=num_pages, page_number=page_number)
 
 
-@app.route('/SVM_SMOTE')
-def SVM_SMOTE():
+@app.route('/tabel_svm_smote')
+def tabel_svm_smote():
     df_preprocessed = dataset()
     X_preprocessed = df_preprocessed['Text']
-
     tfidf_vecto = TfidfVectorizer()
     X = tfidf_vecto.fit_transform(X_preprocessed)
-
     smote = SMOTE()
     X_resample, y_resample = smote.fit_resample(X, df_preprocessed['Label'])
-
     # Split Data
     x_train, x_test, y_train, y_test = train_test_split(
         X_resample, y_resample, train_size=0.8, random_state=42)
-
-    # Modeling
+    # Modeling SVM
     modelSVM_SMOTE = SVC(kernel='linear', probability=True)
     modelSVM_SMOTE.fit(x_train, y_train)
     predictedSVM_SMOTE = modelSVM_SMOTE.predict(x_test)
-
-    # SVM_SMOTE_DF = pd.DataFrame({'Text': [X_preprocessed[i] for i in range(
-    #     len(y_test))], 'Label': y_test, 'SVM_SMOTE': predictedSVM_SMOTE})
-    SVM_SMOTE_DF = pd.DataFrame({'Text': [X_preprocessed[i] for i in range(
-        len(y_test))], 'Label': [df_preprocessed['Label'][i] for i in range(
-            len(y_test))], 'SVM_SMOTE': predictedSVM_SMOTE})
-
-    # set the number of items per page
+    SVM_SMOTE_DF = pd.DataFrame({'Text': [df_preprocessed['Tweet'][i] for i in range(
+        len(y_test))], 'SVM_SMOTE': predictedSVM_SMOTE})
+    # Set Peganation
     per_page = 20
     num_pages = math.ceil(SVM_SMOTE_DF.shape[0]/per_page)
-
-    # get the page number from the request's query parameters
     page_number = int(request.args.get('page', 1))
     if page_number > num_pages:
         page_number = num_pages
@@ -268,7 +229,7 @@ def SVM_SMOTE():
         page_number = 1
     SVM_SMOTE_TABEL = SVM_SMOTE_DF.iloc[per_page *
                                         (page_number-1):per_page*page_number]
-    return render_template('SVM_SMOTE_Ex.html', data=SVM_SMOTE_TABEL, num_pages=num_pages, page_number=page_number)
+    return render_template('tabel_svm_smote.html', data=SVM_SMOTE_TABEL, num_pages=num_pages, page_number=page_number)
 
 
 if __name__ == '__main__':
